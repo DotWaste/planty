088d57a382e01343a82cc2828f7a91ffe1cded11
Thomas Pauly <purplepachiderm@gmail.com>
2023-08-04 17:06:41 +0200

    Correction footer, refonte des pages avec elementor

--- wp-content/themes/planty/footer.php
+++ wp-content/themes/planty/footer.php
@@ -14,19 +14,12 @@ do_action('blocksy:content:bottom');
 
 ?>
 	</main>
-
+	<div class="ftr-bg">
+		<figure id="ftr-img" class="wp-block-image size-large">
+			<img decoding="async" loading="lazy" src="http://localhost/planty/wp-content/uploads/2023/07/footer-image-1-1024x88.png" alt="" srcset="http://localhost/planty/wp-content/uploads/2023/07/footer-image-1-1024x88.png 1024w, http://localhost/planty/wp-content/uploads/2023/07/footer-image-1-300x26.png 300w, http://localhost/planty/wp-content/uploads/2023/07/footer-image-1-768x66.png 768w, http://localhost/planty/wp-content/uploads/2023/07/footer-image-1.png 1489w" sizes="(max-width: 1024px) 100vw, 1024px">
+		</figure>
+	</div>
 	<footer id="footer" class="ct-footer" data-id="type-1" itemscope="" itemtype="https://schema.org/WPFooter" >
-		<div data-row="middle" >
-			<div class="ct-container-fluid" data-columns-divider="md:sm" >
-				<div data-column="widget-area-1" data-link="none" >
-					<div class="ct-widget widget_block widget_media_image" id="block-8">
-					<figure class="wp-block-image size-large">
-						<img class="ftr-img" decoding="async" loading="lazy" src="http://localhost/planty/wp-content/uploads/2023/07/footer-image-1-1024x88.png" alt="" srcset="http://localhost/planty/wp-content/uploads/2023/07/footer-image-1-1024x88.png 1024w, http://localhost/planty/wp-content/uploads/2023/07/footer-image-1-300x26.png 300w, http://localhost/planty/wp-content/uploads/2023/07/footer-image-1-768x66.png 768w, http://localhost/planty/wp-content/uploads/2023/07/footer-image-1.png 1489w" sizes="(max-width: 1024px) 100vw, 1024px" />
-					</figure>
-					</div>
-				</div>
-			</div>
-		</div>
 		<div data-row="bottom" >
 			<div class="ct-container-fluid" data-columns-divider="md:sm" >
 				<div data-column="copyright" >
